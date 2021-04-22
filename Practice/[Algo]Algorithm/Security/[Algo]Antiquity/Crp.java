898ebb7312d6c433188f4f9cba06fb4cf27d9a38
Kim youn ki <rklpoi1234@naver.com>
2021-04-23 00:58:05 +0900

    Algo : Caesar_password_(Encryption) + (Decryption)

--- Practice/[Algo]Algorithm/Security/[Algo]Antiquity/Crp.java
+++ Practice/[Algo]Algorithm/Security/[Algo]Antiquity/Crp.java
@@ -1,7 +1,9 @@
 
-public class Crp {
+public class Crp 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 
         Shift shf = new Shift();
         // 암호화 target 앞문자열 뒤정수(shift)
@@ -9,17 +11,22 @@ public class Crp {
     }
 }
 
-class Shift {
-    public String shift(String s, int n) {
+class Shift 
+{
+    public String shift(String s, int n) 
+    {
         StringBuilder sb = new StringBuilder();
         // 연산시마다 필요한 메모리를 추가적할당 사용
         // https://taiyokara.tistory.com/32
 
-        for (int i = 0; i < s.length(); i++) { // string값길이만큼반복 스트링번지값에서하나씩 캐릭터형ch로변환
+        for (int i = 0; i < s.length(); i++) 
+        { // string값길이만큼반복 스트링번지값에서하나씩 캐릭터형ch로변환
             char ch = s.charAt(i);
-            if (ch > 'A' && ch < 'Z') { // 정수형인지 문자형인지구분을해줘야됩니다.
+            if (ch > 'A' && ch < 'Z') 
+            { // 정수형인지 문자형인지구분을해줘야됩니다.
                 ch = (char) (ch + n);
-                if (ch > 'Z') { // z캐릭터값을 초과했을시 a~z한사이클안 26개의알파벳
+                if (ch > 'Z') 
+                { // z캐릭터값을 초과했을시 a~z한사이클안 26개의알파벳
                     ch -= 26;
                 }
             }
